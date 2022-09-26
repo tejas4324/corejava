1b024f0e7ed06b0adf11110785a0ebef5fb98226
Dell <Dell@DESKTOP-JFCF2E6>
2022-09-26 10:43:16 +0530

    first commit

---
+++ abstraction/src/com/tns/abstraction/Myclass.java
@@ -0,0 +1,10 @@
+package com.tns.abstraction;
+
+public class Myclass {
+	public static void main(String[] args) {
+		Subtraction s=new Subtraction();
+		Multiplication m=new Multiplication();
+		s.calculate(10,5);
+		m.calculate(10,20);
+	}
+}
---
+++ abstraction/src/com/tns/abstraction/Subtraction.java
@@ -0,0 +1,5 @@
+package com.tns.abstraction;
+
+public class Subtraction {
+
+}
