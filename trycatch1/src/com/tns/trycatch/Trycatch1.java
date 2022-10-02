5966273225965829e904c5c5a05bf932664d186e
Dell <Dell@DESKTOP-JFCF2E6>
2022-10-02 19:36:33 +0530

    first commit

---
+++ trycatch1/src/com/tns/trycatch/Trycatch1.java
@@ -0,0 +1,14 @@
+package com.tns.trycatch;
+
+public class Trycatch1 {
+	
+	public static void main(String[] args) {
+		try {
+			int d=0;
+			int s=45/d;
+			System.out.println("will not be printed");
+		}catch(ArithmeticException e){
+			System.out.println("divide with the zero is invalid ");
+			
+		}}
+}
